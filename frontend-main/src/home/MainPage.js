942d55197d70873d0b4be04a9c157b1ddcf68a42
woojung1234 <84664664+woojung1234@users.noreply.github.com>
2025-05-23 18:16:47 +0900

    Add voice input button to home page

--- frontend-main/src/home/MainPage.js
+++ frontend-main/src/home/MainPage.js
@@ -3,6 +3,7 @@ import Header from "home/component/Header";
 import WelfareSlide from "home/component/WelfareSlide";
 import "home/MainPage.css";
 import { useEffect, useState } from "react";
+import VoiceButton from "home/component/VoiceButton"; // 음성입력 버튼 컴포넌트 추가
 
 function MainPage() {
   const [isProtege, setIsProtege] = useState(true);
@@ -10,11 +11,8 @@ function MainPage() {
   useEffect(() => {
     const userType = localStorage.getItem("loginUser");
     
-    if (userType === "PROTEGE") {
-      setIsProtege(true);
-    } else {
-      setIsProtege(false);
-    }
+    // 모든 사용자가 USER 타입이므로 항상 true로 설정
+    setIsProtege(true);
   }, []);
 
   return (
@@ -22,6 +20,8 @@ function MainPage() {
       <Header isProtege={isProtege} />
       <WelfareSlide isProtege={isProtege}/>
       <Button isProtege={isProtege} />
+      {/* 음성입력 버튼 추가 */}
+      <VoiceButton />
     </div>
   );
 }
